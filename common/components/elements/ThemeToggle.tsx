8fb919780d7666faa0aaad175fa07b8e541044c8
Sahil Shrestha <sahiltuchhe123@gmail.com>
2025-04-21 23:28:24 +0545

    Updated readme

--- common/components/elements/ThemeToggle.tsx
+++ common/components/elements/ThemeToggle.tsx
@@ -2,13 +2,21 @@
 
 import { useTheme } from "next-themes";
 import { BsCloudMoon, BsCloudSun } from "react-icons/bs";
+  import { useEffect, useState } from "react";
 
 const ThemeToggle = () => {
   const { resolvedTheme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   const toggleTheme = () => {
     setTheme(resolvedTheme === "light" ? "dark" : "light");
   };
+
+  if (!mounted) return null; // Prevent rendering until mounted
 
   return (
     <button
