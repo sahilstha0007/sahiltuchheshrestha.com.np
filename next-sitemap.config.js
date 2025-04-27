5e3fe5aa003d71029d83ae16fa4ffad61a9faf01
Sahil Shrestha <sahiltuchhe123@gmail.com>
2025-04-27 21:13:27 +0545

    analytics

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  siteUrl: process.env.DOMAIN || "https://sahilshrestha.com",
-  generateRobotsTxt: true,
-  generateIndexSitemap: true,  // Important ✅
-  sitemapSize: 7000,
-  outDir: "./public",           // Important ✅
-};
-
-export default config;
